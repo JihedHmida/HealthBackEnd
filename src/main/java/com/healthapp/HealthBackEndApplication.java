681268b91f464b5eab38d6fbe1624503a4623d09
Jihed Hmida <JihedHmida@users.noreply.github.com>
2018-12-17 04:06:54 +0100

    Update .gitignore

---
+++ src/main/java/com/healthapp/HealthBackEndApplication.java
@@ -0,0 +1,14 @@
+package com.healthapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthBackEndApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/healthapp/HealthBackEndApplicationTests.java
@@ -0,0 +1,17 @@
+package com.healthapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HealthBackEndApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
